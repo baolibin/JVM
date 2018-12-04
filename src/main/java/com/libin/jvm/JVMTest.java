510a74afdfae9385e15e5d8f53ba599acfa36cc2
baolibin <2578858653@qq.com>
2018-12-04 21:02:22 +0800

    jvm

---
+++ src/main/java/com/libin/jvm/JVMTest.java
@@ -0,0 +1,10 @@
+package com.libin.jvm;
+
+/**
+ * Copyright (c) 2018/12/04. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class JVMTest {
+}
